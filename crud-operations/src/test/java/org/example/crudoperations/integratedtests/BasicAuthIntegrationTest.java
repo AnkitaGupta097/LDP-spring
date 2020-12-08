ff68a57a789bb5adeee3c3082658e3c4b2a881f9
Ankita <ankita.gupta@zemosolabs.com>
2020-12-08 23:00:41 +0530

    integration test with h2 database

--- crud-operations/src/test/java/org/example/crudoperations/integratedtests/BasicAuthIntegrationTest.java
+++ crud-operations/src/test/java/org/example/crudoperations/integratedtests/BasicAuthIntegrationTest.java
@@ -3,16 +3,11 @@ package org.example.crudoperations.integratedtests;
 import org.example.crudoperations.CrudOperationsApplication;
 import org.example.crudoperations.entity.Role;
 import org.example.crudoperations.entity.User;
-import org.example.crudoperations.repository.RoleRepository;
-import org.example.crudoperations.repository.UserRepository;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
@@ -20,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CrudOperationsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -32,24 +26,11 @@ public class BasicAuthIntegrationTest {
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @MockBean
-    UserRepository userRepository;
-
-    @MockBean
-    RoleRepository roleRepository;
 
     TestRestTemplate restTemplate=new TestRestTemplate();
 
     HttpHeaders headers=new HttpHeaders();
-    User user;
 
-    @Before
-    public void setup(){
-        user=new User("admin","email",bCryptPasswordEncoder.encode("admin"), Arrays.asList(new Role("ROLE_ADMIN")));
-
-        Mockito.when(userRepository.findByUserId(Mockito.eq("admin"))).thenReturn(Optional.of(user));
-
-    }
 
     @Test
     public void testRetrieveCurrentUserDetail()  {
@@ -61,6 +42,8 @@ public class BasicAuthIntegrationTest {
                 HttpMethod.GET, entity, String.class);
 
         Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assert.assertNotNull(response.getBody());
+
 
     }
 
@@ -86,6 +69,22 @@ public class BasicAuthIntegrationTest {
                 HttpMethod.GET, entity, String.class);
 
         Assert.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+
+    }
+    @Test
+    public void testSignUp()  {
+        User testUser=new User("ankita","email","ankita", Arrays.asList(new Role("ROLE_USER")));
+        HttpEntity<User> entity = new HttpEntity<>(testUser, headers);
+
+
+        ResponseEntity<User> response = restTemplate.exchange(
+                createURLWithPort("/signup"),
+                HttpMethod.POST, entity, User.class);
+
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assert.assertEquals(testUser.getUserId(), response.getBody().getUserId());
+
+
 
     }
 
